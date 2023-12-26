3f974bee0c53e6217a2808616352313abfddb65b
kilicseyit <seyit.kilic1@outlook.com>
2023-12-26 15:54:39 +0300

    VueSlot
    VueJs'in sahip olduğu vue slot özelliğini öğrendik.

---
+++ vue3-slot/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <alt-bilesen>
+    <template v-slot:form-header>
+      <h2>Giriş Formu</h2>
+    </template>
+
+    <template v-slot:form-elements>
+      <label for="email">E-Posta</label>
+      <input type="email" v-model="email" placeholder="E-posta adresinizi giriniz">
+
+      <label for="password">Şifre</label>
+      <input type="password" v-model="password" placeholder="Şifrenizi giriniz">
+      <h2>E-postanız : {{ email }}</h2>
+      <h2>Şifreniz : {{ password }}</h2>
+    </template>
+
+    <template v-slot:form-control>
+      <button>Giriş Yap</button>
+    </template>
+  </alt-bilesen>
+</template>
+
+<script>
+import altBilesen from './components/alt-bilesen.vue';
+
+export default {
+  components: {
+    "alt-bilesen": altBilesen
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  }
+}
+</script>
---
+++ vue3-slot/src/components/alt-bilesen.vue
@@ -0,0 +1,45 @@
+<template>
+    <form action="#">
+        <div class="container">
+            <div class="container-inner">
+                <div class="form-header">
+                    <slot name="form-header"></slot>
+                </div>
+
+                <div class="form-elements">
+                    <div class="form-elements-inner">
+                        <slot name="form-elements"></slot>
+                </div>
+
+                <div class="form-controls">
+                    <div class="form-controls-inner"></div>
+                         <slot name="form-controls"></slot>   
+                    </div>
+
+                </div>
+
+    
+            </div>
+        </div>
+    </form>
+
+</template>
+
+<style scoped>
+.container{widows: 100%;margin:0 auto;}
+.container-inner{width: 50%; margin: 0px auto; border: 1px solid #e2e2e2; padding: 15px;}
+.form-header{text-align: center; border-bottom: 1px solid #e2e2e2; margin-top: 35px;}
+.form-elements{display: flex; justify-content: center; margin-top: 35px;}
+.form-elements-inner{width: 50%;}
+:slotted(input){width: 100%; border: 1px solid #e2e2e2; height: 35px; border-radius: 5px;border-radius: 5px; margin: 5px 0 15px 0; padding-left: 15px;}
+.form-controls{display: flex; justify-content: center; text-align: right; margin: 15px;}
+.form-controls-inner{width: 50%;}
+:slotted(button){height: 35px; border:1px solid #e2e2e2; padding: 0px 15;}
+.useful-links{display: flex; justify-content: center; border-top: 1px solid #e2e2e2;}
+ul{list-style-type: none;margin-top: 15px 0 0 0;}
+ul li{float: left; margin-right: 5px;}
+ul li a{text-decoration: none; color:#666;}
+ul li a :hover{color:#000}
+
+
+</style>
